dfc9ac2f89e1920205c267f8f6aa6bc52e897e16
Chen Lei <chenlei03@newhuotech.com>
2023-11-03 18:55:26 +0800

    增加了启动时输入了错误密码的提示信息

--- env/production/node.sh
+++ env/production/node.sh
@@ -259,6 +259,8 @@ init|info|reset|start)
         RESPONSE=$(curl -s -w "\n%{http_code}" --location --request GET "http://${UNSEAL_SERVER}/sys/seal-status" \
           --header 'Content-Type: text/plain' \
           --data "")
+      else
+        echo "Invalid password, please try again."
       fi
     fi
 
